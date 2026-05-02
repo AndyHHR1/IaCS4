44af511087422154c78cfdebec69f119bd6fb3fb
AndyHHR1 <andyhermenegildo25@gmail.com>
2026-05-02 11:25:58 -0500

    feat: lamba crop y upload

---
+++ tf/terraform/lb_crop.tf
@@ -0,0 +1,34 @@
+data "archive_file" "crop_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda-crop"
+  output_path = "${path.module}/crop_function.zip"
+}
+
+resource "aws_lambda_function" "crop" {
+  function_name    = "crop-lambda-${var.environment}"
+  role             = aws_iam_role.lambda_common_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs20.x"
+  memory_size      = 512
+  timeout          = 60
+  filename         = data.archive_file.crop_zip.output_path
+  source_code_hash = data.archive_file.crop_zip.output_base64sha256
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+    security_group_ids = [aws_vpc.main.default_security_group_id]
+  }
+
+  environment {
+    variables = {
+      S3_BUCKET        = aws_s3_bucket.images.id
+      PROCESSED_PREFIX = "processed/"
+    }
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn = aws_sqs_queue.image_queue.arn
+  function_name    = aws_lambda_function.crop.arn
+  batch_size       = 5
+}
